50846007cf4dc1e7c4e0ffbbae205c0ec09174d3
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2022-03-02 12:08:14 -0500

    router configuration

--- ADOPT-ME/src/App.js
+++ ADOPT-ME/src/App.js
@@ -1,11 +1,19 @@
 import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { StrictMode } from "react";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 const App = () => {
   return (
     <StrictMode>
       <div>
-        <h1>Adopt Me!</h1>
+        <BrowserRouter>
+          <h1>Adopt Me!</h1>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </BrowserRouter>
         <SearchParams />
       </div>
     </StrictMode>
