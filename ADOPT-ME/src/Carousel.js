d14826fc89adcb645994a7b2b9c6f4e9e997de7c
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2022-03-02 14:47:41 -0500

    handle cliicking of the images of pets

--- ADOPT-ME/src/Carousel.js
+++ ADOPT-ME/src/Carousel.js
@@ -9,6 +9,12 @@ class Carousel extends Component {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
 
+  handleIndexClick = (event) => {
+    this.setState({
+      active: +event.target.dataset.index,
+    });
+  };
+
   render() {
     const { active } = this.state;
     const { images } = this.props;
@@ -19,8 +25,10 @@ class Carousel extends Component {
           {images.map((photo, index) => (
             // eslint-disable-next-line
             <img
+              onClick={this.handleIndexClick}
               key={photo}
               src={photo}
+              data-index={index}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
             />
